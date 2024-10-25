49bede669d126a08a8da893f9da60b6f8b77e62e
404975-Zabala-joaquin <404975@tecnicatura.frc.utn.edu.ar>
2024-10-25 18:00:37 -0300

    post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BudgetFormComponent } from './budget-form/budget-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   
+    {path: '', component:BudgetFormComponent},
+];
